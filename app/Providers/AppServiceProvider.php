cfaa48c72ab16f84a95b1b9ca7f250d7335e85d5
Inna Tarasyan <innatarasyanmail@gmail.com>
2022-08-23 21:37:40 +0400

    Laravel Contracts Implementation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Contracts\UsersRepository;
+use App\Contracts\UsersRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(UsersRepositoryInterface::class, function () {
+            return new UsersRepository();
+        });
     }
 
     /**
